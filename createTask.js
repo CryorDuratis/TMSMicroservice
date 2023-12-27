8eb3e4d5810a92bf307b38a2466350779cc728ab
Bryce <bryce.tan.jw@gmail.com>
2023-12-27 17:05:24 +0800

    finalised structure

--- createTask.js
+++ createTask.js
@@ -1,11 +1,28 @@
+// 1. endpoint exist checked in app.js
+// 2. endpoint correct checked in app.js
+
 exports.CreateTask = async (req, res) => {
   try {
-    const x = res.body
-    // som
-    return res.json({
-      code: x
+    // define request body parameters
+    const { username, password, app_acronym, task_name } = req.body
+
+    // 3. mandatory fields present
+    // 4. valid data types
+    // 5. valid user credentials
+    // 6. active user
+    // 7. app exists
+    // 8. permitted user
+    // 9. sql check
+
+    res.json({
+      code: "x"
     })
-  } catch (error) {
-    // something
+  } catch (e) {
+    console.log(e)
+    // 10. catch other errors
+    // async error
+    return res.json({
+      code: "T701"
+    })
   }
 }
